0fc5d9e5b9eb9a073ac506aeccea70f9e98796af
T0astBread <tb@t0ast.cc>
2022-08-03 19:30:07 +0200

    Actual pages with Markdown

--- build/index.ts
+++ build/index.ts
@@ -6,8 +6,69 @@ import * as signal from "./signal"
 import * as sse from "./sse"
 import { CleanupHandler } from "./cleanup"
 import * as handlebars from "handlebars"
+import { marked } from "marked"
+import * as event from "node:events"
 
 const startTime = new Date()
+
+const watchClient = `
+<script>
+const startTime = ${JSON.stringify(startTime)}
+
+async function waitForServer(timeout) {
+    document.title = "Waiting for server..."
+    // await new Promise(r => setTimeout(r, timeout))
+
+    while (true) {
+        // const ac = new AbortController()
+        // setTimeout(() => ac.abort(), 100)
+        try {
+            const response = await fetch("/start-time", {
+                // signal: ac.signal,
+            })
+            const text = await response.text()
+            if (text !== startTime)
+                break
+            await new Promise(r => setTimeout(r, timeout))
+        } catch {
+            await new Promise(r => setTimeout(r, timeout))
+        }
+    }
+}
+
+const evts = new EventSource("/evt")
+evts.addEventListener("compiling", () => {
+    document.title = "Compiling..."
+})
+evts.addEventListener("build-failure", () => {
+    document.title = "Build failure :<"
+})
+evts.addEventListener("something", evt => {
+    console.log(evt.data)
+    const p = document.createElement("p")
+    p.innerText = evt.data
+    document.body.appendChild(p)
+})
+evts.addEventListener("refresh", async () => {
+    evts.close()
+    console.info("Waiting for the server with a LOW TIMEOUT")
+    await waitForServer(10)
+    location.reload()
+})
+evts.addEventListener("reload", async () => {
+    evts.close()
+    location.reload()
+})
+evts.addEventListener("error", async evt => {
+    evts.close()
+    console.error("Error in event connection")
+    console.info("Waiting for the server with a HIGH TIMEOUT")
+    await waitForServer(3000)
+    location.reload()
+})
+</script>
+`
+
 const cleanup = new CleanupHandler()
 log.info("Hi!")
 cleanup.register(() => {
@@ -15,125 +76,86 @@ cleanup.register(() => {
     log.debug()
 })
 
-async function buildHB() {
+async function buildPages() {
     const hb = handlebars.create()
     const xSrc = await fs.readFile("components/x.hbs", "utf-8")
     hb.registerPartial("x", hb.compile(xSrc))
 
-    const pages = {
-        "idk": hb.compile("{{#> x}}hello there{{/x}}")({})
-    }
-    return pages
+    const pageRenderPromises = (await fs.readdir("pages"))
+        .map(async fileName => {
+            const src = await fs.readFile(`${process.cwd()}/pages/${fileName}`, "utf-8")
+            const md = hb.compile(src)({})
+            const html = marked(md)
+            return {
+                [fileName.replace(/\.md$/, ".html")]: html,
+            }
+        })
+
+    return (await Promise.all(pageRenderPromises))
+        .reduce((a, b) => ({...a, ...b}), {})
 }
-let pages = buildHB()
+let pages = buildPages()
 
 log.debug("Starting watcher...")
-const watcher = syncfs.watch("components", async (type, file) => {
-    pages = buildHB()
-    await eventServer.broadcast("reload", null)
-})
-cleanup.register(() => {
-    log.debug("Closing watcher...")
-    watcher.close()
-    log.debug("Closed watcher")
-})
+function startWatching(dirName: string) {
+    const watcher = syncfs.watch(dirName, async (type, file) => {
+        pages = buildPages()
+        await eventServer.broadcast("reload", null)
+    })
+    cleanup.register(() => {
+        log.debug("Closing watcher...")
+        watcher.close()
+        log.debug("Closed watcher")
+    })
+}
+startWatching("components")
+startWatching("pages")
+// const watcher = syncfs.watch("components", async (type, file) => {
+//     pages = buildPages()
+//     await eventServer.broadcast("reload", null)
+// })
+// cleanup.register(() => {
+//     log.debug("Closing watcher...")
+//     watcher.close()
+//     log.debug("Closed watcher")
+// })
 log.info("Started watcher")
 
 const eventServer = new sse.EventServer()
 
+function resolvePath(requestURL: string | undefined) {
+    switch(requestURL) {
+        case undefined:
+        case "/":
+            return "index.html"
+        default:
+            return requestURL.substring(1) + ".html"
+    }
+}
+
 const server = http.createServer(async (request, response) => {
     log.debug("Serving", request.method, request.url)
 
-    if (request.url === "/") {
-        response.writeHead(200, {
-            "Content-Type": "text/html;charset=utf-8",
-        })
-
-        response.write(`
-            ${(await pages).idk}
-
-            <script>
-                const startTime = ${JSON.stringify(startTime)}
-
-                async function waitForServer(timeout) {
-                    document.title = "Waiting for server..."
-                    // await new Promise(r => setTimeout(r, timeout))
-
-                    while (true) {
-                        // const ac = new AbortController()
-                        // setTimeout(() => ac.abort(), 100)
-                        try {
-                            const response = await fetch("/start-time", {
-                                // signal: ac.signal,
-                            })
-                            const text = await response.text()
-                            if (text !== startTime)
-                                break
-                            await new Promise(r => setTimeout(r, timeout))
-                        } catch {
-                            await new Promise(r => setTimeout(r, timeout))
-                        }
-                    }
-                }
-
-                const evts = new EventSource("/evt")
-                evts.addEventListener("compiling", () => {
-                    document.title = "Compiling..."
-                })
-                evts.addEventListener("build-failure", () => {
-                    document.title = "Build failure :<"
-                })
-                evts.addEventListener("something", evt => {
-                    console.log(evt.data)
-                    const p = document.createElement("p")
-                    p.innerText = evt.data
-                    document.body.appendChild(p)
-                })
-                evts.addEventListener("refresh", async () => {
-                    evts.close()
-                    console.info("Waiting for the server with a LOW TIMEOUT")
-                    await waitForServer(10)
-                    location.reload()
-                })
-                evts.addEventListener("reload", async () => {
-                    evts.close()
-                    location.reload()
-                })
-                evts.addEventListener("error", async evt => {
-                    evts.close()
-                    console.error("Error in event connection")
-                    console.info("Waiting for the server with a HIGH TIMEOUT")
-                    await waitForServer(3000)
-                    location.reload()
-                })
-            </script>
-        `)
-        response.end()
-    } else if (request.url === "/start-time") {
+    if (request.url === "/start-time") {
         response.writeHead(200, {
             "Content-Type": "text/plain",
         })
 
         response.end(startTime.toJSON())
     } else if (request.url === "/evt") {
-        const sendEvent = eventServer.startSending(response)
-
-        // const interval = setInterval(() => {
-        //     log.debug("Sending event")
-        //     if (response.closed) {
-        //         log.debug("Sending event - closed!")
-        //         clearInterval(interval)
-        //         return
-        //     }
-        //     sendEvent("something", "hi from the server!")
-        // }, 500)
-        // response.once("close", () => {
-        //     log.debug("Stopping events - closed")
-        //     clearInterval(interval)
-        // })
+        void eventServer.startSending(response)
     } else {
-        response.writeHead(404, "Not Found")
-        response.end()
+        // const url = new URL(request.url ?? "/")
+        const pathName = resolvePath(request.url)
+        log.debug("Resolved path", request.url, "to", pathName)
+        const page = (await pages)[pathName]
+        if (page) {
+            response.writeHead(200, "Cool choice!")
+            response.end(page + "\n\n" + watchClient)
+        } else {
+            response.writeHead(404, "Not Found")
+            response.end(watchClient)
+        }
     }
 })
 cleanup.register(async () => {
---
+++ pages/index.md
@@ -0,0 +1,11 @@
+# hey!
+
+{{#> x}}
+    also works
+
+# heading inside a thing
+{{/x}}
+
+```js
+function foo () {}
+```
